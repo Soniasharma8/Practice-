02f963550b552a0eb6f498f16d70ca348d0a7737
Sonia Sharma <Soniashar2021@gmail.com>
2023-12-24 21:26:10 +0530

    Java\Day-2\addnumbers.java

---
+++ Java/Day-2/addnumbers.java
@@ -0,0 +1,15 @@
+// add three numbers in Java Program
+
+public class addnumbers {
+    
+    public static void main(String[] args) {
+        // write your code here
+
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+        int sum = num1 + num2 + num3;
+        
+        System.out.println(sum);
+    }
+}
